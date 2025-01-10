014c7709cc8de7f56ab24003e441e5d5cc06e36f
Sam-24-dev <scaizapa@espol.edu.ec>
2025-01-09 21:36:40 -0500

    Diseño de la Interfaz
    Actualización de varios archivos ,para que sea 100% el juego en la Interfaz Grafica usando JavaFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,8 @@
-module ec.edu.espol.singleplayertictactoe {
+module ec.edu.espol.singleplayer.tictactoe {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
-    opens ec.edu.espol.singleplayertictactoe to javafx.fxml;
-    exports ec.edu.espol.singleplayertictactoe;
+    requires java.base;
+    
+    opens ec.edu.espol.singleplayer.tictactoe to javafx.fxml;
+    exports ec.edu.espol.singleplayer.tictactoe;
 }
